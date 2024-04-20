23781afb41cb4aff7815f1f3572f67650cafc59e
Juan Carlos Fernandez <142681063+juancarlosfdzcode@users.noreply.github.com>
2024-04-20 20:03:27 +0200

    Add files via upload

---
+++ jsCode/landing.js
@@ -0,0 +1,9 @@
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.landing i', {origin:'bottom'});
+ScrollReveal().reveal('.landing h1, h2', {origin:'left'});
