92fc760631aa989cb3695b87b310c076eefd3a88
Juan Carlos Fernandez <142681063+juancarlosfdzcode@users.noreply.github.com>
2024-04-20 19:38:21 +0200

    Add files via upload

---
+++ jsCode/projects.js
@@ -0,0 +1,9 @@
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.design-content', {origin:'right'});
+ScrollReveal().reveal('.blog-item', {origin:'top'});
